eda84bd0ed2663e2a09cc6be00576f64e97878f2
tanius <matthias@ansorgs.de>
2018-04-01 01:25:53 +0100

    first functional coffee image splitter

--- scripts/create-bean-images.py
+++ scripts/create-bean-images.py
@@ -1,27 +1,72 @@
 import cv2
 import numpy as np
 
-# Load the image
-img = cv2.imread('coffee.jpg') # TODO pass image filename from command line
+## Configuration sectionself.
+resolution = 8.0 # px/mm
 
-# convert to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img_width  = 1200
+img_height = 1600
+# TODO Determine the image dimensions from the given image.
 
-# smooth the image to avoid noises
-gray = cv2.medianBlur(gray, 5)
+# Output image width and height.
+# (Output images should cover a physical size of 14.35*14.35 mm always.)
+img_target_size = int(14.35 * resolution)
 
-# Apply adaptive threshold
-thresh = cv2.adaptiveThreshold(gray, 255, 1, 1, 11, 2)
+# Load the image.
+# TODO Pass image filename from command line.
+img = cv2.imread('coffee.jpg')
 
-# Find the contours
-contours,hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+# Convert to grayscale.
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# For each contour, find the bounding rectangle and draw it
+# Smooth the image to avoid noises.
+# TODO Blur radius should be much higher (factor 200 for a 1200x1600 image), and adapted to the image size.
+img_gray = cv2.medianBlur(img_gray, 5)
+
+# Apply adaptive threshold.
+# Reference: https://docs.opencv.org/3.4.0/d7/d1b/group__imgproc__misc.html#ga72b913f352e4a1b1b397736707afcde3
+# TODO Calculate the adaptiveThreshold blocksize parameter as 25% of larger side of image.
+img_bw = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 401, 0)
+
+# Find the contours.
+# Reference: https://docs.opencv.org/3.4.0/d3/dc0/group__imgproc__shape.html#ga17ed9f5d79ae97bd4c7cf18403e1689a
+contours = cv2.findContours(img_bw, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[1]
+
+# Convert the image from gray to color mode, so we can draw in color on it later.
+img_bw = cv2.cvtColor(img_bw, cv2.COLOR_GRAY2BGR)
+
+# For each contour, find the bounding box, draw it, save it.
+img_num = 1
 for cnt in contours:
     x,y,w,h = cv2.boundingRect(cnt)
-    cv2.rectangle(img, (x,y), (x+w,y+h), (0, 255, 0), 2) # TODO saving the image instead of painting the bounding box
 
-# Finally show the image
-cv2.imshow('img', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # Draw a green bounding box around the contour.
+    cv2.rectangle(img_bw, (x,y), (x+w, y+h), (0,255,0), 2)
+
+    # Skip fragments smaller than beans.
+    # TODO Adapt the pixel count threshold based on image dimensions.
+    if (h*w < 3000): continue
+
+    # TODO Grow the bounding box to a square of the right (resolution dependent) "standard size".
+    center_x = x + w//2
+    center_y = y + h//2
+    x = max(center_x - (img_target_size//2), 0)
+    y = max(center_y - (img_target_size//2), 0)
+    w = img_target_size
+    if (x+w > img_width): w = img_width - x
+    h = img_target_size
+    if (y+h > img_height): h = img_height - y
+
+    # Extract the bounding box content ("region of interest", hopefully a bean)
+    roi = img[y:y+h, x:x+w]
+
+    # TODO If the bounding box is smaller than the "standard size", pad it with white background.
+
+    # Save the ROI as image.
+    # TODO Derive the output file names from input file names.
+    cv2.imwrite("bean_" + str(img_num).zfill(2) + ".png", roi)
+    img_num += 1
+
+# Save the b&w image with bounding boxes as visual control.
+# TODO Only do this when required by a --debug command line argument.
+cv2.imwrite('coffee.rois.png', img_bw)
